fea973f2ce718f22c0057f5e362f0aa9e686f4b2
Oscar Tsang <ohhtsang@gmail.com>
2020-05-08 16:49:58 +0100

    added new components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,18 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <person-form></person-form>
+    <food-form></food-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import foodForm from '@/components/foodForm.vue'
+import personForm from '@/components/personForm.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    'food-form': foodForm,
+    'person-form': personForm
   }
 }
 </script>
---
+++ client/src/components/personForm.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <form v-on:submit.prevent="newPerson" method="post">
+    <label>Name:</label>
+    <input type="text" v-model="name">
+
+    <label>Gender:</label>
+    <select v-model="gender">
+      <option disabled on value>Select a gender</option>
+      <option value="male" >Male</option>
+      <option value="female" >Female</option>
+    </select>
+
+    <label>Age:</label>
+    <input type="number" v-model="age">
+
+    <label>Height (cm):</label>
+    <input type="numbers" v-model="height">
+
+    <label>Weight (kg):</label>
+    <input type="numbers" v-model="weight">
+
+    <input type="submit">
+
+  </form>
+</template>
+
+<script>
+import trackerService from '@/services/trackerService.js';
+
+export default {
+  name: 'tracker-form',
+  data(){
+    return {
+      name: '',
+      gender: '',
+      age: null,
+      height: null,
+      weight: null,
+      dailyRequiredCalories: null
+    }
+  },
+  methods: {
+    newPerson(){
+      const newPerson = {
+        name: this.name,
+        gender: this.gender,
+        height: this.height,
+        weight: this.weight,
+        calories: 2750
+      }
+      trackerService.postPersonData(newPerson)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
