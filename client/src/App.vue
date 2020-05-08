7f07d0e3ab10cc89b4eb26f1e89d165bdd394471
Oscar Tsang <ohhtsang@gmail.com>
2020-05-08 17:02:22 +0100

    add entries form to component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,23 @@
 <template lang="html">
   <div>
     <person-form></person-form>
+    <br>
+    <br>
     <food-form></food-form>
+    <br>
+    <br>
+    <entries-form></entries-form>
   </div>
 </template>
 
 <script>
 import foodForm from '@/components/foodForm.vue'
 import personForm from '@/components/personForm.vue'
+import entriesForm from '@/components/entriesForm.vue';
 export default {
   name: 'app',
   components: {
+    'entries-form': entriesForm,
     'food-form': foodForm,
     'person-form': personForm
   }
---
+++ client/src/components/entriesForm.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <form v-on:submit.prevent="addFoodToEntries" method="post">
+    <label for="">Food name:</label>
+    <input type="text" v-model="name">
+
+    <label for="">Calories</label>
+    <input type="number" v-model="calories">
+
+    <input type="submit" value="Add food to entry">
+  </form>
+</template>
+
+<script>
+import trackerService from '@/services/trackerService.js'
+export default {
+  name: 'food-form',
+  data(){
+    return {
+      name: '',
+      calories: null
+    }
+  },
+  methods: {
+    addFoodToEntries(){
+      const newFood = {
+        name: this.name,
+        calories: this.calories
+      }
+      trackerService.postFoodData(newFood)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
