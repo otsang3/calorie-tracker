d73df5f87b6a5246d9d4ceded54f01e2d1bbeeb6
Oscar Tsang <ohhtsang@gmail.com>
2020-05-10 17:18:22 +0100

    add files

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -15,18 +15,7 @@ db.person.insertMany([
 ]);
 
 db.meals.insertMany([
-  {
-    date: "08/05/2020",
-    breakfast: {sausages: 100},
-    lunch: {burger: 200},
-    dinner: {chicken: 100}
-  },
-  {
-    date: "09/05/2020",
-    breakfast: {beans: 100},
-    lunch: {peas: 200},
-    dinner: {haggis: 100}
-  }
+{"_id":{"$oid":"5eb70393266522068d93c912"},"date":"09-05-2020","caloriesLeft":-669,"caloriesEntered":4500,"breakfast":{"oatmeal":500,"banana":100,"porridge":360,"apple":230,"pear":100,"grape":50},"lunch":{"chicken":500,"burger":600,"sandwich":100,"yoghurt":50,"bacon":10,"bread":30},"dinner":{"steak":1000,"lamb":550,"pork":300,"avocado":20}}
 ]);
 
 db.foods.insertMany([
