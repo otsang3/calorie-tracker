e14c908b0f94a267e576bcda70732b7d40b2d8b4
Oscar Tsang <ohhtsang@gmail.com>
2020-05-10 19:09:46 +0100

    minor changes

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -15,7 +15,33 @@ db.person.insertMany([
 ]);
 
 db.meals.insertMany([
-{"_id":{"$oid":"5eb70393266522068d93c912"},"date":"09-05-2020","caloriesLeft":-669,"caloriesEntered":4500,"breakfast":{"oatmeal":500,"banana":100,"porridge":360,"apple":230,"pear":100,"grape":50},"lunch":{"chicken":500,"burger":600,"sandwich":100,"yoghurt":50,"bacon":10,"bread":30},"dinner":{"steak":1000,"lamb":550,"pork":300,"avocado":20}}
+  {
+    _id:"5eb70393266522068d93c912",
+    date:"07-05-2020",
+    caloriesLeft:-669,
+    caloriesEntered:4500,
+    breakfast:{oatmeal:500,banana:100,porridge:360,apple:230,pear:100,grape:50},
+    lunch:{chicken:500,burger:600,sandwich:100,yoghurt:50,bacon:10,bread:30},
+    dinner:{steak:1000,lamb:550,pork:300,avocado:20}
+  },
+  {
+    _id:"5eb70393266522068d93c913",
+    date:"08-05-2020",
+    caloriesLeft:-669,
+    caloriesEntered:4500,
+    breakfast:{oatmeal:500,banana:100,porridge:360,apple:230,pear:100,grape:50},
+    lunch:{chicken:500,burger:600,sandwich:100,yoghurt:50,bacon:10,bread:30},
+    dinner:{steak:1000,lamb:550,pork:300,avocado:20}
+  },
+  {
+    _id:"5eb70393266522068d93c914",
+    date:"09-05-2020",
+    caloriesLeft:-669,
+    caloriesEntered:4500,
+    breakfast:{oatmeal:500,banana:100,porridge:360,apple:230,pear:100,grape:50},
+    lunch:{chicken:500,burger:600,sandwich:100,yoghurt:50,bacon:10,bread:30},
+    dinner:{steak:1000,lamb:550,pork:300,avocado:20}
+  },
 ]);
 
 db.foods.insertMany([
