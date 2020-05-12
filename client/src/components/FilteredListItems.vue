7a5a4575d7a3973f7c447db01433f3de6d28fe3a
h0usemaintenance <h0usemaintenance@hotmail.com>
2020-05-12 20:14:18 +0100

    Resolving filter form issue.

--- client/src/components/FilteredListItems.vue
+++ client/src/components/FilteredListItems.vue
@@ -3,20 +3,26 @@
     <p>Date: {{meal.date}}</p>
     <p>Calories left: {{meal.caloriesLeft}}</p>
     <p>Calories added: {{meal.caloriesEntered}}</p>
-    <p v-if="breakfast"><h4>Breakfast</h4></p>
-    <div v-for="[key, value] of Object.entries(meal.breakfast)">
-      {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[0])">update</button>
-      <button v-on:click="deleteMeal(key, mealTypes[0])">delete</button>
+    <div v-if="meal.breakfast">
+      <h4>Breakfast</h4>
+      <div v-for="[key, value] of Object.entries(meal.breakfast)">
+        {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[0])">update</button>
+        <button v-on:click="deleteMeal(key, mealTypes[0])">delete</button>
+      </div>
     </div>
-    <p v-if="lunch"><h4>Lunch</h4></p>
-    <div v-for="[key, value] of Object.entries(meal.lunch)">
-      {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[1])">update</button>
-      <button v-on:click="deleteMeal(key, mealTypes[1])">delete</button>
+    <div v-else-if="meal.lunch">
+      <h4>Lunch</h4>
+      <div v-for="[key, value] of Object.entries(meal.lunch)">
+        {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[1])">update</button>
+        <button v-on:click="deleteMeal(key, mealTypes[1])">delete</button>
+      </div>
     </div>
-    <p v-if="dinner"><h4>Dinner</h4></p>
-    <div v-for="[key, value] of Object.entries(meal.dinner)">
-      {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[2])">update</button>
-      <button v-on:click="deleteMeal(key, mealTypes[2])">delete</button>
+    <div v-else-if="meal.dinner">
+      <h4>Dinner</h4>
+      <div v-for="[key, value] of Object.entries(meal.dinner)">
+        {{key}} : <input type="number" v-model="value"><button v-on:click="updateMeal(key, value, mealTypes[2])">update</button>
+        <button v-on:click="deleteMeal(key, mealTypes[2])">delete</button>
+      </div>
     </div>
   </div>
 </template>
@@ -42,39 +48,23 @@ export default {
       this.mealObjectId = this.meal._id;
     }
   },
-  computed: {
-    breakfast(){
-      return this.extractMeals(this.mealTypes[0]);
-    },
-    lunch(){
-      return this.extractMeals(this.mealTypes[1]);
-    },
-    dinner(){
-      return this.extractMeals(this.mealTypes[2]);
-    }
-  },
   methods: {
-    extractMeals(type){
-      if (this.meal[type]) {
-        return (Object.keys(this.meal[type]).map((key, index) => [key, Object.values(this.meal[type])[index]])).flat();
-      }
-    },
     deleteMeal(mealKey, mealType){
       const updatedObject = this.meal;
       delete updatedObject._id;
       const mealKeyValue = updatedObject[mealType][mealKey];
       delete updatedObject[mealType][mealKey];
-      updatedObject.caloriesEntered -= mealKeyValue;
-      updatedObject.caloriesLeft += mealKeyValue;
+      updatedObject.caloriesEntered -= parseInt(mealKeyValue);
+      updatedObject.caloriesLeft += parseInt(mealKeyValue);
       TrackerService.updateMealDetails(updatedObject, this.mealObjectId)
       .then((meal) => eventBus.$emit('meal-item-deleted', meal));
     },
     updateMeal(mealKey, mealValue, mealType){
       const currentKeyValue = this.meal[mealType][mealKey]
       this.meal[mealType][mealKey] = parseInt(mealValue);
-      this.meal.caloriesLeft += currentKeyValue - mealValue;
+      this.meal.caloriesLeft += parseInt(currentKeyValue - mealValue);
       const calculatedValue = currentKeyValue < mealValue ? mealValue - currentKeyValue : mealValue - currentKeyValue;
-      this.meal.caloriesEntered += calculatedValue;
+      this.meal.caloriesEntered += parseInt(calculatedValue);
       const updatedObject = this.meal;
       delete updatedObject._id;
       TrackerService.updateMealDetails(updatedObject, this.mealObjectId)
